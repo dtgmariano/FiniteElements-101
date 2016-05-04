b81d7ef18aa579aea1cce412cd027cf11f46b6dd
Daniel Mariano <dtgm@uol.com.br>
2016-05-04 11:53:18 -0300

    ...

--- Studies/LuaStudy103/study103.lua
+++ Studies/LuaStudy103/study103.lua
@@ -11,16 +11,23 @@
 --function name: draw_square
 --input arguments: home, length
 --output arguments: none
-function draw_square(home, length)
-  mi_addnode(home[1]          , home[2]         )
-  mi_addnode(home[1]          , home[2] + length)
-  mi_addnode(home[1] + length , home[2] + length)
-  mi_addnode(home[1] + length , home[2]         )
+function draw_outer_square()
+  mi_addnode(0   ,   0)
+  mi_addnode(0   , 254)
+  mi_addnode(203 , 254)
+  mi_addnode(203 ,   0)
 
-  mi_addsegment(home[1]          , home[2]          , home[1]          , home[2] + length)
-  mi_addsegment(home[1]          , home[2] + length , home[1] + length , home[2] + length)
-  mi_addsegment(home[1] + length , home[2] + length , home[1] + length , home[2]         )
-  mi_addsegment(home[1] + length , home[2]          , home[1]          , home[2]         )
+  mi_addsegment(0  ,   0,   0, 254)
+  mi_addsegment(0  , 254, 203, 254)
+  mi_addsegment(203, 254, 203,   0)
+  mi_addsegment(203,   0,   0,   0)
+end
+
+function draw_inner_square()
+  mi_addnode(51  ,  50)
+  mi_addnode(152 ,  50)
+  mi_addnode(51  , 204)
+  mi_addnode(152 , 204)
 end
 
 --%%%%%%%%%%%%%%%%--
@@ -32,11 +39,7 @@ newdocument(0)
 mi_probdef(0,'centimeters','planar',1e-8,3,3.28,30)
 mi_saveas("study102.fem")
 
-points = {{0, 0}, {0, 1}, {1, 1}, {1, 0}}
-draw_points(points)
-
-position = {3, 3}
-size = 1
-draw_square(position,size)
+draw_inner_square()
+draw_outer_square()
 
 mi_clearselected()
