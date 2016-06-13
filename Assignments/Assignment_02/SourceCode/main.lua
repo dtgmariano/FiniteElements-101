38d8f09422a0f33fc268bd59a0aef734df5cc680
Daniel Mariano <dtgm@uol.com.br>
2016-06-13 11:58:54 -0300

    source code

--- Assignments/Assignment_02/SourceCode/main.lua
+++ Assignments/Assignment_02/SourceCode/main.lua
@@ -1,38 +1,3 @@
---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%--
---% Auhtor: Daniel Teodoro Gonçalves Mariano                  %--
---% Date: 04/05/2016                                          %--
---% Description: Program to develop functions for LUA script  %--
---%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%--
-
---%%%%%%%%%%%%%--
---% Functions %--
---%%%%%%%%%%%%%--
-
---function name: draw_square
---input arguments: points
---output arguments: none
-function draw_square(points)
-  --Nodes
-  for i=1, 4 do
-    mi_addnode(points[i][1], points[i][2])
-  end
-
-  --Segments
-  mi_addsegment(points[1][1], points[1][2], points[2][1], points[2][2])
-  mi_addsegment(points[2][1], points[2][2], points[3][1], points[3][2])
-  mi_addsegment(points[3][1], points[3][2], points[4][1], points[4][2])
-  mi_addsegment(points[4][1], points[4][2], points[1][1], points[1][2])
-end
-
-
-function draw_circle(points)
-  for i=1, 2 do
-    mi_addnode(points[i][1], points[i][2])
-  end
-  mi_addarc(points[1][1], points[1][2], points[2][1], points[2][2], 180, 1)
-  mi_addarc(points[2][1], points[2][2], points[1][1], points[1][2], 180, 1)
-end
-
 --%%%%%%%%%%%%%%%%--
 --% Main Program %--
 --%%%%%%%%%%%%%%%%--
@@ -42,17 +7,23 @@ end
   mi_probdef(0 ,'millimeters','planar','1E-8','4','30',0)
   --mi_saveas("study102.fem")
 
+  local funcs = require "functions.lua"
+
   --Drawing Points and Segments
   outerpoints = {{0,0} , {203.2,0}, {203.2,254}, {0,254}}
-  draw_square(outerpoints)
+  funcs.draw_square(outerpoints)
   innerpoints = {{50.8,50} , {152.4,50}, {152.4,204}, {50.8,204}}
-  draw_square(innerpoints)
+  funcs.draw_square(innerpoints)
   coil1 = {{-2,124.5}, {0, 124.5}, {0, 129.5}, {-2, 129.5}}
-  draw_square(coil1)
+  funcs.draw_square(coil1)
+  --mi_setgroup(1)
+  --mi_clearselected()
   coil2 = {{50.8,124.5} , {52.8,124.5}, {52.8,129.5}, {50.8,129.5}}
-  draw_square(coil2)
+  funcs.draw_square(coil2)
+  --mi_setgroup(2)
+  --mi_clearselected()
   circ = {{-200,127}, {403.2,127}}
-  draw_circle(circ)
+  funcs.draw_circle(circ)
 
   --Condition Border
 	mi_selectsegment(circ[1][1],circ[1][2])
