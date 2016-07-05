56617320506fa2932cbf7df0e52c5773c669896d
Daniel Mariano <dtgm@uol.com.br>
2016-07-05 01:15:58 -0300

    assign4

--- Assignments/Assignment_04/SourceCode/Library/plots.lua
+++ Assignments/Assignment_04/SourceCode/Library/plots.lua
@@ -4,6 +4,9 @@ function do_plots()
   mo_addcontour(-600,0)
   mo_addcontour(600,0)
   mo_makeplot(1,40000) -- 1: Plot |B|
+  mo_savebitmap("plot_b.bmp")
   mo_makeplot(0,40000) -- 0: Plot Potential
+  mo_savebitmap("plot_p.bmp")
   mo_makeplot(4,40000) -- 4: Plot |H|
+  mo_savebitmap("plot_h.bmp")
 end
